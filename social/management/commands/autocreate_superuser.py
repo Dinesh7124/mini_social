b7bf403157b479f6dd0cc500cb0ffe09b2b42df4
Dinesh7124 <support10@idsolutionsindia.com>
2026-09-24 09:03:19 +0530

    Fix command location - remove duplicate

--- social/management/commands/autocreate_superuser.py
+++ social/management/commands/autocreate_superuser.py
@@ -1,4 +1,3 @@
-# social/management/commands/autocreate_superuser.py
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 import os
@@ -9,8 +8,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin@12345')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'support10@idsolutionsindia.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Dinesh@7124')
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(
